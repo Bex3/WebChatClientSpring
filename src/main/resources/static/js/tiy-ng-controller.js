17e82d49b349f7d07414a23767a78f253f1f2ca6
bex3 <Rebeccam.bearden@gmail>
2016-09-25 11:26:46 -0400

    up

--- src/main/resources/static/js/tiy-ng-controller.js
+++ src/main/resources/static/js/tiy-ng-controller.js
@@ -5,7 +5,7 @@ angular.module('TIYChatApp', [])
             console.log("About to go get me some data!");
             $scope.name = "JavaScript Master Guru";
 
-            $http.get("//localhost:8080/games.json") //asynch call
+            $http.get("//localhost:8080/messages.json") //asynch call
                 .then( //provides the callback
                     function successCallback(response) { //inside of the promise object then holds the 2 functions
                         console.log(response.data); //data is the json object as a javascript object
@@ -17,3 +17,21 @@ angular.module('TIYChatApp', [])
                     });
                     console.log("Done with the promise - waiting for the callback");
         };
+
+                $scope.addMessage = function() {
+                    console.log("About to add the following game " + JSON.stringify($scope.newMessage));
+
+                    $http.post("/addMessage.json", $scope.newMessage)
+                        .then(
+                            function successCallback(response) {
+                                console.log(response.data);
+                                console.log("Adding data to scope");
+                                $scope.messages = response.data;
+                            },
+                            function errorCallback(response) {
+                                console.log("Unable to get data");
+                            });
+                };
+
+                $scope.newMessage = {};
+   });
